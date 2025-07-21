ca9a644efe4dc55384f1222316ec39a117ea85fa
David <awesomez0legendz@gmail.com>
2025-07-20 21:35:43 -0400

    header files

---
+++ game.h
@@ -0,0 +1,26 @@
+#ifndef GAME_H
+#define GAME_H
+//#include "display.h"
+#include "player.h"
+//#include "subject.h"
+
+class Game {
+
+public:
+
+    void playGame() {}
+
+    void doCommand(char a, char b, int c) {}
+
+    Player getActivePlayer() {}
+
+    void drawCard() {}
+
+    void discard(int i) {}
+
+    void quit() {}
+
+};
+
+#endif
+
---
+++ player.h
@@ -0,0 +1,15 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+//#include "collection.h"
+class Player {
+    int health;
+    int mana;
+public:
+
+    void takeDamage(int dmg) {}
+
+    int getHandSize() {}
+
+};
+
+#endif
